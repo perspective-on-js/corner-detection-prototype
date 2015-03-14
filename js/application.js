530037b1e0d0b89c1103ae5c0f38a083c0d73b12
dvberkel <daan.v.berkel.1980@gmail.com>
2015-03-14 08:17:22 +0100

    Load image into canvas

--- js/application.js
+++ js/application.js
@@ -1,3 +1,16 @@
 ;(function(){
-    console.log('ready to go');
+    var canvas = document.getElementById('canvas');
+
+    function loadImage(url, callback){
+	var image = new Image();
+	image.onload = function(){
+	    callback(this);
+	}
+	image.src = url;
+    }
+
+    loadImage('/image/sample.jpg', function(image){
+	var context = canvas.getContext('2d');
+	context.drawImage(image, 0, 0);
+    });
 })()
